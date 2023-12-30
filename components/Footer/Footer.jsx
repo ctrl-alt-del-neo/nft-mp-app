7913fb59b6817173db6b0cbbd2a6934ab8b44c25
gaby <gabriel@xmanna.com>
2023-12-30 17:01:26 +0200

    Navbar component coding complete

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// INTERNAL IMPORT
+import Style from './Footer.module.css'
+
+const Footer = () => {
+
+    // INTERNAL IMPORT
+
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
